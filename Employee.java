6c8a90c78f1d9142eab9fcf3b3987e3e9a035b14
ShauliR <shauliriger@gmail.com>
2017-02-07 20:44:00 +0200

    test 2

---
+++ Employee.java
@@ -0,0 +1,65 @@
+package db;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEES")
+public class Employee implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private long id;
+	
+	@Column(name="firstName")
+	private String firstName;
+	
+	@Column(name="lastName")
+	private String lastName;
+	
+	@Column(name="salary")
+	private double salary;
+	
+	@Column(name="dept")
+	private String dept;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	
+	
+}
+
